6fcaad773ab6051697c2fdc54528705453166b5f
lucassingh <lucas.singh10@gmail.com>
2025-08-28 19:29:25 -0300

    add regions

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    assetsInclude: ['**/*.ttf'],
-    build: {
-        chunkSizeWarningLimit: 1000,
-    }
+    assetsInclude: ['**/*.ttf']
 })
